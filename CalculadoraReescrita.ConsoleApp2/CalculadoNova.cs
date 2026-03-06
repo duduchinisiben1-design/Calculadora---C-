af15169a80f849b8355d9fa2b57fac6d4887d015
Carlos Eduardo Chini Siben <duduchinisiben1@gmail.com>
2026-03-06 16:19:22 -0300

    Adiconal sobre o histórico de operações

--- CalculadoraReescrita.ConsoleApp2/CalculadoNova.cs
+++ CalculadoraReescrita.ConsoleApp2/CalculadoNova.cs
@@ -1,129 +1,169 @@
-﻿bool deveContinuar = true;
-while (deveContinuar == true)
+﻿
+string[] historicoOperacoes = new string[10];
+
+
+int contadorOperacoes = 0;
+
+while (true == true)
 {
     Console.WriteLine("-------------------------------");
-Console.WriteLine("Seja Bem-Vindo à Calculadora Resumida, 2026");
-Console.WriteLine("-------------------------------");
+    Console.WriteLine("Seja Bem-Vindo à Calculadora Resumida, 2026");
+    Console.WriteLine("-------------------------------");
 
-Console.WriteLine("Clique ENTER para que possamos resolver seu problema matemático");
-Console.ReadLine();
+    Console.WriteLine("Clique ENTER para que possamos resolver seu problema matemático");
+    Console.ReadLine();
 
-Console.WriteLine("Após a sua recepção calarosa da nossa desenvolvedora, escolha uma destas opções abaixo");
+    Console.WriteLine("Após uma recepção calarosa da nossa desenvolvedora, escolha uma destas opções abaixo");
 
-Console.WriteLine();
+    Console.WriteLine();
 
-Console.WriteLine("----------------------------------------");
-Console.WriteLine("Qual o tipo do seu problema matemático?");
-Console.WriteLine("----------------------------------------");
+    Console.WriteLine("----------------------------------------");
+    Console.WriteLine("Qual o tipo do seu problema matemático?");
+    Console.WriteLine("----------------------------------------");
 
-Console.WriteLine();
+    Console.WriteLine();
 
-Console.WriteLine("1 - Adição");
-Console.WriteLine("2 - Subtração");
-Console.WriteLine("3 - Multiplicação");
-Console.WriteLine("4 - Divisão");
-Console.WriteLine("5 - Tabuada");
-Console.WriteLine("S - Finalizar o programa");
+    Console.WriteLine("1 - Adição");
+    Console.WriteLine("2 - Subtração");
+    Console.WriteLine("3 - Multiplicação");
+    Console.WriteLine("4 - Divisão");
+    Console.WriteLine("5 - Tabuada");
+    Console.WriteLine("6 - Hitórico de Operações");
+    Console.WriteLine("S - Finalizar o programa");
 
-Console.WriteLine();
+    Console.WriteLine();
 
-Console.WriteLine("Relembrando, Selecione uma opção válida.");
-string? operacaoSelecionada = Console.ReadLine();
+    Console.WriteLine("Relembrando, Selecione uma opção válida.");
+    string? operacaoSelecionada = Console.ReadLine();
 
-Console.WriteLine();
+    Console.WriteLine();
 
-int? primeiroNumero;
-int? segundoNumero;
-
-decimal? resultado;
-
-
-switch (operacaoSelecionada)
-{
-    case "1": 
-        Console.WriteLine("Digite o primeiro número da sua conta: ");
-        primeiroNumero = Convert.ToInt32(Console.ReadLine());
-
-        Console.WriteLine();
-
-        Console.WriteLine("Digite o segundo número da sua conta: ");
-        segundoNumero = Convert.ToInt32(Console.ReadLine());
-
-        Console.WriteLine();
+    string textoOperacao = "";
+    
 
 
-        resultado = primeiroNumero + segundoNumero;
-        Console.WriteLine("O resultado da sua conta seria: " + resultado);
+    int? primeiroNumero;
+    int? segundoNumero;
 
-    break;
-    case "2":
-        Console.WriteLine("Digite o primeiro numero da sua conta: ");
-        primeiroNumero = Convert.ToInt32(Console.ReadLine());
+    decimal? resultado;
 
-        Console.WriteLine();
 
-        Console.WriteLine("Digite o segundo número da sua conta: ");
-        segundoNumero = Convert.ToInt32(Console.ReadLine());
+    switch (operacaoSelecionada)
+    {
+        case "1":
+            Console.WriteLine("Digite o primeiro número da sua conta: ");
+            primeiroNumero = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine();
+            Console.WriteLine();
 
-        resultado = primeiroNumero - segundoNumero;
-        Console.WriteLine("O resultado da sua conta seria: " + resultado);
-    break;
-    case "3": 
-        Console.WriteLine("Digite o primeiro numero da sua conta: ");
-        primeiroNumero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número da sua conta: ");
+            segundoNumero = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine();
+            Console.WriteLine();
 
-        Console.WriteLine("Digite o segundo número da sua conta: ");
-        segundoNumero = Convert.ToInt32(Console.ReadLine());
+            
+            resultado = primeiroNumero + segundoNumero;
+            textoOperacao = $"{primeiroNumero} + {segundoNumero} = {resultado}";
+            Console.WriteLine("O resultado da sua conta seria: " + resultado);
 
-        Console.WriteLine();
+            break;
+        case "2":
+            Console.WriteLine("Digite o primeiro numero da sua conta: ");
+            primeiroNumero = Convert.ToInt32(Console.ReadLine());
 
-        resultado = primeiroNumero * segundoNumero;
-        Console.WriteLine("O resultado da sua conta seria: " + resultado);
-    break;
-    case "4": 
-        Console.WriteLine("Digite o primeiro numero da sua conta: ");
-        primeiroNumero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
 
-        Console.WriteLine();
+            Console.WriteLine("Digite o segundo número da sua conta: ");
+            segundoNumero = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine("Digite o segundo número da sua conta: ");
-        segundoNumero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
 
-        Console.WriteLine();
+            resultado = primeiroNumero - segundoNumero;
+            textoOperacao = $"{primeiroNumero} - {segundoNumero} = {resultado}";
+            Console.WriteLine("O resultado da sua conta seria: " + resultado);
+            break;
+        case "3":
+            Console.WriteLine("Digite o primeiro numero da sua conta: ");
+            primeiroNumero = Convert.ToInt32(Console.ReadLine());
 
-        if (segundoNumero == 0)
-        {
-            Console.WriteLine("Nenhum número é divisível por 0!!");
-        }
+            Console.WriteLine();
 
-        resultado = primeiroNumero / segundoNumero;
-        Console.WriteLine("O resultado da sua conta seria: " + resultado);
-    break;
-    case "5":
-        Console.WriteLine("Digite o numero da tabuada desejada: ");
-        decimal numeroTabuada = Convert.ToDecimal(Console.ReadLine());
-        decimal resultadoTabuada;
+            Console.WriteLine("Digite o segundo número da sua conta: ");
+            segundoNumero = Convert.ToInt32(Console.ReadLine());
 
-        for (int i = 0; i <= 10; i++)
-        {
-            resultadoTabuada = numeroTabuada * i;
-            Console.WriteLine(numeroTabuada + " X " + i + " = " + resultadoTabuada);
+            Console.WriteLine();
 
-        }
-    break;
-    case "S":
-        Console.WriteLine("Seu Programa Foi Finalizado Com Sucesso!!");
+            
+            resultado = primeiroNumero * segundoNumero;
+            textoOperacao = $"{primeiroNumero} * {segundoNumero} = {resultado}";
+            Console.WriteLine("O resultado da sua conta seria: " + resultado);
+            break;
+        case "4":
+            Console.WriteLine("Digite o primeiro numero da sua conta: ");
+            primeiroNumero = Convert.ToInt32(Console.ReadLine());
 
-        
-    return;
-    default:
-        Console.WriteLine("Opção Não Encontrda!!");
-    break;
-}
+            Console.WriteLine();
+
+            Console.WriteLine("Digite o segundo número da sua conta: ");
+            segundoNumero = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            if (segundoNumero == 0)
+            {
+                Console.WriteLine("Nenhum número é divisível por 0!!");
+            }
+
+            resultado = primeiroNumero / segundoNumero;
+            textoOperacao = $"{primeiroNumero} / {segundoNumero} = {resultado}";
+            Console.WriteLine("O resultado da sua conta seria: " + resultado);
+            break;
+        case "5":
+            Console.WriteLine("Digite o numero da tabuada desejada: ");
+            decimal numeroTabuada = Convert.ToDecimal(Console.ReadLine());
+            decimal resultadoTabuada;
+
+            for (int i = 0; i <= 10; i++)
+            {
+                resultadoTabuada = numeroTabuada * i;
+                Console.WriteLine(numeroTabuada + " X " + i + " = " + resultadoTabuada);
+
+            }
+            break;
+        case "S":
+            Console.WriteLine("Seu Programa Foi Finalizado Com Sucesso!!");
+            return;
+        case "6":
+
+            Console.WriteLine("Histórico de Operações:");
+            Console.WriteLine("---------------------------------");
+
+            for (int contador = 0; contador < contadorOperacoes; contador++)
+            {
+                Console.WriteLine(historicoOperacoes[contador]);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Clique ENTER para que vc possa voltar ao inicio do programa!!");
+
+            Console.ReadLine();
+
+            continue;
+        default:
+            Console.WriteLine("Opção Não Encontrda!!");
+            continue;
+
+            
+
+    }
+
+    if (contadorOperacoes < historicoOperacoes.Length)
+            {
+                historicoOperacoes[contadorOperacoes] = textoOperacao;
+
+                contadorOperacoes++;
+                
+            }
 }
 
 
